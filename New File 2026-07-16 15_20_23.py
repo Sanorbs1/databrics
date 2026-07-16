a76801c89fc6e6ccff08abd4dde64b0d8334376c
Sanorbs1 <sandeep.b@diggibyte.com>
2026-07-16 15:51:36 +0530

    Add docstrings, type hints, f-strings

--- New File 2026-07-16 15_20_23.py
+++ New File 2026-07-16 15_20_23.py
@@ -8,3 +8,25 @@ def fun(n):
     return n*fun(n-1)
 
 print(fun(n))
+
+"Add docstrings : this file is for docstring practice"
+def square(number: int) -> int:
+    """
+    Returns the square of a number.
+
+    Args:
+        number: Input integer.
+
+    Returns:
+        Square of the integer.
+    """
+    return number * number
+
+"type hints"
+def add(a: int, b: int) -> int:
+    return a + b
+
+"F-strings"
+salary = 50000
+
+print(f"Salary is {salary}")
